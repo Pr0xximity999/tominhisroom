8d43d87a5e3ddfa45d605af3de8eaca3d43683d7
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-07-12 18:27:30 +0200

    temp removal for main push

--- website/scripts/quoter.js
+++ website/scripts/quoter.js
@@ -26,20 +26,20 @@ var index = Math.floor(Math.random() * qoutes.length);
 
 quoteText.textContent = ">" + qoutes[index];
 
-// Select the quoteText element
-var quoteText = document.getElementById("quoteText");
+// // Select the quoteText element
+// var quoteText = document.getElementById("quoteText");
 
 
 
-fetch(`https://ori.ivorylotus.dev/zin/all`)
-    .then(response => response.json())
-    .then(data => {
-        var quotes = data.sentences;
-        var index = Math.floor(Math.random() * qoutes.length);
+// fetch(`https://ori.ivorylotus.dev/zin/all`)
+//     .then(response => response.json())
+//     .then(data => {
+//         var quotes = data.sentences;
+//         var index = Math.floor(Math.random() * qoutes.length);
 
-        quoteText.textContent = ">" + qoutes[index];
-    })
-    .catch(error => {
-        quoteText.textContent = ">And i watched the water unfold~";
-    })
+//         quoteText.textContent = ">" + qoutes[index];
+//     })
+//     .catch(error => {
+//         quoteText.textContent = ">And i watched the water unfold~";
+//     })
 
